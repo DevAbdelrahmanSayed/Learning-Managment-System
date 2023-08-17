49c597493b4351133175c827ec1807dc33311ca7
shehab <shehabwaleed2010@gmail.com>
2023-08-18 01:51:30 +0300

    Edit validation in RegisterController

--- Modules/Auth/Http/Controllers/RegisterController.php
+++ Modules/Auth/Http/Controllers/RegisterController.php
@@ -3,13 +3,14 @@
 namespace Modules\Auth\Http\Controllers;
 
 use App\Helpers\ApiResponse;
-use App\Mail\EmailVerification;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailVerification;
 use Illuminate\Validation\Rule;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Modules\Teacher\Entities\Teacher;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Contracts\Support\Renderable;
 use Modules\Auth\Transformers\TeacherResource;
@@ -21,25 +22,33 @@ class RegisterController extends Controller
 
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'type' => ['required', Rule::in(['teacher', 'student'])],
+        $validator = Validator::make($request->all(), [
+            'type' => ['required', Rule::in(['teacher', 'student'])]
         ]);
+
+        if($validator->fails())
+            return ApiResponse::sendResponse(422, 'Validation Errors', $validator->errors());
+
         return ($request->type === 'teacher') ? $this->storeTeacher($request) : $this->storeStudent($request);
     }
 
 
     public function storeTeacher(Request $request)
     {
-        $teacherData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'name' => ['required', 'string', 'min:3', 'max:25'],
             'email' => ['required', 'email', 'unique:teachers,email'],
             'password' => ['required', 'max:255', Password::defaults()]
         ]);
-        $teacherData = Teacher::create($teacherData);
+
+        if ($validator->fails())
+            return ApiResponse::sendResponse(422, 'Validation Errors', $validator->errors());
+
+        $teacherData = Teacher::create($validator->validated());
         $this->otpGenerate($teacherData);
         $teacherData['token'] = JWTAuth::fromUser($teacherData);
 
-        Mail::to( $teacherData->email)->send(new EmailVerification($teacherData->otp,$teacherData->name));
+        Mail::to($teacherData->email)->send(new EmailVerification($teacherData->otp, $teacherData->name));
         return ApiResponse::sendResponse(201, 'Teacher Account Created Successfully', new TeacherResource($teacherData));
     }
 
